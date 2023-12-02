0e55be7e838530dfc7bcf9fc0b26f0a9e7163b50
Suraj Das <surajdas1907@gmail.com>
2023-12-02 18:23:30 +0530

    Optimal Approach is added

--- Arrays/zeroToEnd.cpp
+++ Arrays/zeroToEnd.cpp
@@ -56,15 +56,67 @@ void moveZeroToEnd(vector<int> &arr)
     }
 }
 
+// Optimal Appraoch
+
+/*
+    Approach
+    first step
+    --1.1st initialize j as -1 and find  where the 1st zero element lies
+    --2.This can be done by simply iterating over an array an check if arr[i]==0.
+    --3.if it is zero then state j=i and break
+
+    second step
+    --1.once we have the first zero element right after that we have to place i
+    --2 Then simply check if a[i]!=0 swap arr[i] with arr[j] n increment i
+
+    time complxity :O(N);
+    Space Complexity:O(1);
+
+*/
+
+void moveZeroEnd(vector<int> &brr)
+{
+
+    // finding where 1st zero lies
+    int j = -1;
+
+    for (int i = 0; i < brr.size(); i++)
+    {
+        if (brr[i] == 0)
+        {
+            j = i;
+            break;
+        }
+    }
+    // placing i right after the j
+
+    for (int i = j + 1; i < brr.size(); i++)
+    {
+        if (brr[i] != 0)
+        {
+            swap(brr[i], brr[j]);
+            j++;
+        }
+    }
+}
 int main()
 {
 
     vector<int> arr = {1, 0, 2, 3, 0, 4, 0, 1};
     int n = arr.size();
-    moveZeroToEnd(arr);
+    // moveZeroToEnd(arr);
+    // cout << "After Moving Zeros to the end:" << endl;
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    vector<int> brr = {1, 2, 0, 1, 0, 4, 0};
+    int m = brr.size();
+    moveZeroEnd(brr);
     cout << "After Moving Zeros to the end:" << endl;
-    for (int i = 0; i < arr.size(); i++)
+    for (int i = 0; i < brr.size(); i++)
     {
-        cout << arr[i] << " ";
+        cout << brr[i] << " ";
     }
 }
