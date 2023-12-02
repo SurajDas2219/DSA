c6158c8d2ce45adfb1f63d34a7f2039fb06b0bee
Suraj Das <surajdas1907@gmail.com>
2023-12-02 18:44:47 +0530

    Factorial of the given number

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findFactorial(int n)
+{
+    // Base Case
+    if (n == 1)
+    {
+        return 1;
+    }
+    // Rcursive Relation
+    return n * findFactorial(n - 1);
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter the number whose factorial is to be found:" << endl;
+    cin >> n;
+
+    int ans = findFactorial(n);
+    cout << "The Factorail of " << n << " is " << ans;
+
+    return 0;
+}
